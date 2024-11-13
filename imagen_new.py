40f2113d0486dfa47770898c1afe019825bc48b0
javiervzpucp <jveraz@pucp.edu.pe>
2024-11-13 17:47:37 -0500

    Update imagen_new.py

--- imagen_new.py
+++ imagen_new.py
@@ -109,7 +109,7 @@ else:
             st.write(description)
             
             # Guardar la nueva descripción en el DataFrame y en el archivo CSV
-            new_df = new_df.append({"imagen": img_url, "descripción": title, "generated_description": description}, ignore_index=True)
+            new_df = new_df.append({"imagen": img_url, "descripcion": title, "generated_description": description}, ignore_index=True)
             new_df.to_csv(new_dataset_path, sep=';', index=False)
 
 # Mostrar el historial de descripciones generadas
